90b12e8458822944045cac0d81456c5d564c1879
ccirnoo <13798571373@163.com>
2020-08-11 13:26:48 +0800

    first commit

---
+++ src/vuex/store.js
@@ -0,0 +1,42 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+Vue.use(Vuex);
+
+let state = JSON.parse(localStorage.getItem('state')) || {
+  userInfo: {},
+  ifLogin: false,
+  newsList: [],
+  loading: false
+};
+
+const mutations = {
+  userInfo(state, payload) {
+    state.userInfo = payload;
+    localStorage.setItem('state', JSON.stringify(state));
+  },
+  ifLogin(state, payload) {
+    if (!payload) state.userInfo = '';
+    state.ifLogin = payload;
+    localStorage.setItem('state', JSON.stringify(state));
+  },
+  msgCount(state, payload) {
+    state.userInfo.tt_count = payload;
+    localStorage.setItem('state', JSON.stringify(state));
+  },
+  articleCount(state, payload) {
+    state.userInfo.article_count = payload;
+    localStorage.setItem('state', JSON.stringify(state));
+  },
+  newsList(state, payload) {
+    state.newsList = payload;
+    localStorage.setItem('state', JSON.stringify(state));
+  },
+  loading(state, payload) {
+    state.loading = payload;
+  },
+};
+
+export default new Vuex.Store({
+  state,
+  mutations
+})
